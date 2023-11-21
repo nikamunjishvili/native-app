630c2517393d96f409a2d1236d993e45417cb746
nika <nikamunja31415@gmail.com>
2023-11-21 18:48:55 +0400

    inital app

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello From React Native App!!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+});
+
+export default App;
---
+++ src/utils/common.ts
@@ -0,0 +1,3 @@
+export const prettyPrint = (param: any) => {
+    console.log(JSON.stringify(param, null, 2));
+  };
