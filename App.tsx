cceb85d63872189cd4b8cb7976281408cf6dbf25
nika <nikamunja31415@gmail.com>
2023-11-23 17:26:11 +0400

    add redux in my project

--- App.tsx
+++ App.tsx
@@ -6,15 +6,19 @@
  */
 
 import React from 'react';
-import { StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import DashboardTabRoutes from './src/navigations/dashboard/DashboardTabRoutes';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './src/store/store/index';
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-    <DashboardTabRoutes />
-  </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <DashboardTabRoutes />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
@@ -23,7 +27,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
 });
 
--- src/components/layouts/HomeScreenLayout.tsx
+++ src/components/layouts/HomeScreenLayout.tsx
@@ -1,10 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {decrement, increment} from '../../store/slices';
 
 const HomeScreenLayout = () => {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.container}>
-      <Text>HomeScreenLayout</Text>
+      <Text style={{fontSize: 40}}>{count}</Text>
+      <TouchableOpacity onPress={() => dispatch(increment())}>
+        <Text>Increment</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => dispatch(decrement())}>
+        <Text>Decrement</Text>
+      </TouchableOpacity>
     </View>
   );
 };
